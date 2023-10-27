424721158f5783ef0f090547e833bd0ed3812c5f
kartikdhumal <106480114+kartikdhumal@users.noreply.github.com>
2023-10-27 09:45:32 +0530

    Update Login.jsx result

--- client/src/admin/Login.jsx
+++ client/src/admin/Login.jsx
@@ -26,7 +26,7 @@ function Login() {
            if(email == "" && password== "")
            { alert("Please enter your Email or Password"); }
             else{
-            const response = await Axios.post('netflix-clone-liart-mu-79.vercel.app
+            const response = await Axios.post('netflix-clone-alpha-pearl.vercel.app
 /login', {
               email: email,
               password: password
